d723dda1db810ac159f76bd697a5f4ba59458dab
Mark Cyster <mcyster@gmail.com>
2025-06-08 14:14:06 -0700

    Refine module info for weather modules

--- weather/src/main/java/module-info.java
+++ weather/src/main/java/module-info.java
@@ -1,10 +1,13 @@
 module com.cyster.weather {
-    requires spring.web;
-    requires spring.beans;
-    requires spring.context;
-    requires org.springframework.ai.model;
-    requires com.fasterxml.jackson.databind;
+  requires spring.web;
+  requires spring.beans;
+  requires spring.context;
+  requires com.fasterxml.jackson.databind;
 
-    exports com.cyster.weather.service;
-    opens com.cyster.weather.impl to spring.core, spring.beans, spring.context;
+  exports com.cyster.weather.service;
+
+  opens com.cyster.weather.impl to
+      spring.core,
+      spring.beans,
+      spring.context;
 }
--- weather/weather-tool/src/main/java/module-info.java
+++ weather/weather-tool/src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.cyster.weather.tool {
-    requires com.cyster.weather;
-    requires org.springframework.ai.autoconfigure.model.tool;
-    requires spring.context;
+  requires com.cyster.weather;
+  requires spring.ai.model;
+  requires spring.context;
 
-    exports com.cyster.weather.tool;
-    opens com.cyster.weather.tool to spring.core, spring.beans, spring.context;
+  exports com.cyster.weather.tool;
+
+  opens com.cyster.weather.tool to
+      spring.core,
+      spring.beans,
+      spring.context;
 }
