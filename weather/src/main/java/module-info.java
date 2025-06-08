4ddb12d5037be22b7a2e703effa1325b20c7fb92
Mark Cyster <mcyster@gmail.com>
2025-06-08 12:19:14 -0700

    Fix WeatherService module

--- weather/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.cyster.weather {
-    requires spring.web;
-    requires spring.context;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.annotation;
-
-    exports com.cyster.weather.service;
-    exports com.cyster.weather.impl;
-}
